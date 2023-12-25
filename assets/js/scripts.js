65fc5c67976ca559052b823409a183f27eb0b3e3
doduchuyhuy <doduchuyhuy@gmail.com>
2023-12-25 23:51:29 +0700

    lan4home

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,26 +1,35 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+/**
+ * Hàm tải template
+ *
+ * Cách dùng:
+ * <div id="parent"></div>
+ * <script>
+ *  load("#parent", "./path-to-template.html");
+ * </script>
+ */
 function load(selector, path) {
     const cached = localStorage.getItem(path);
     if (cached) {
-        document.querySelector(selector).innerHTML = cached;
+        $(selector).innerHTML = cached;
     }
 
     fetch(path)
         .then((res) => res.text())
         .then((html) => {
             if (html !== cached) {
-                document.querySelector(selector).innerHTML = html;
+                $(selector).innerHTML = html;
                 localStorage.setItem(path, html);
             }
+        })
+        .finally(() => {
+            window.dispatchEvent(new Event("template-loaded"));
         });
 }
 
-/**
- * JS toggle
- *
- * Cách dùng:
- * <button class="js-toggle" toggle-target="#box">Click</button>
- * <div id="box">Content show/hide</div>
- */
+
 window.addEventListener("template-loaded", initJsToggle);
 
 function initJsToggle() {
