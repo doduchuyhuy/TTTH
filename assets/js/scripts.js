89637b7a24a5fad8f75d9dc6c621ab12647c357b
doduchuyhuy <doduchuyhuy@gmail.com>
2024-01-01 23:06:35 +0700

    lan18home

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,83 +1,67 @@
-const $ = document.querySelector.bind(document);
-const $$ = document.querySelectorAll.bind(document);
+              
+// const $ = document.querySelector.bind(document);
+// const $$ = document.querySelectorAll.bind(document);
 
-/**
- * Hàm tải template
- *
- * Cách dùng:
- * <div id="parent"></div>
- * <script>
- *  load("#parent", "./path-to-template.html");
- * </script>
- */
-function load(selector, path) {
-    const cached = localStorage.getItem(path);
-    if (cached) {
-        $(selector).innerHTML = cached;
-    }
+// /**
+//  * Hàm tải template
+//  *
+//  * Cách dùng:
+//  * <div id="parent"></div>
+//  * <script>
+//  *  load("#parent", "./path-to-template.html");
+//  * </script>
+//  */
+// function load(selector, path) {
+//     const cached = localStorage.getItem(path);
+//     if (cached) {
+//         $(selector).innerHTML = cached;
+//     }
 
-    fetch(path)
-        .then((res) => res.text())
-        .then((html) => {
-            if (html !== cached) {
-                $(selector).innerHTML = html;
-                localStorage.setItem(path, html);
-            }
-        })
-        .finally(() => {
-            window.dispatchEvent(new Event("template-loaded"));
-        });
-}
-
-
-window.addEventListener("template-loaded", initJsToggle);
-
-function initJsToggle() {
-    $$(".js-toggle").forEach((button) => {
-        const target = button.getAttribute("toggle-target");
-        if (!target) {
-            document.body.innerText = `Cần thêm toggle-target cho: ${button.outerHTML}`;
-        }
-        button.onclick = () => {
-            if (!$(target)) {
-                return (document.body.innerText = `Không tìm thấy phần tử "${target}"`);
-            }
-            const isHidden = $(target).classList.contains("hide");
-
-            requestAnimationFrame(() => {
-                $(target).classList.toggle("hide", !isHidden);
-                $(target).classList.toggle("show", isHidden);
-            });
-        };
-    });
-}
-
-window.onload = function(){ 
-    // Lấy phần Modal
-var modal = document.getElementById('myModal');
-// Lấy đường dẫn của hình ảnh và gán vào trong phần Modal
-var images = document.getElementsByClassName('myImg');
-var modalImg = document.getElementById("img01");
-for (var i = 0; i < images.length; i++) {
-    var img = images[i];
-    // and attach our click listener for this image.
-    img.onclick = function(evt) {
-      modal.style.display = "block";
-      modalImg.src = this.src;
-    }
-  }
-// lấy button span có chức năng đóng Modal
-// var span = document.getElementsByClassName("close")[0];
-//Khi button được click, đóng modal
-// span.onclick = function() { 
-//     modal.style.display = "none";
+//     fetch(path)
+//         .then((res) => res.text())
+//         .then((html) => {
+//             if (html !== cached) {
+//                 $(selector).innerHTML = html;
+//                 localStorage.setItem(path, html);
+//             }
+//         })
+//         .finally(() => {
+//             window.dispatchEvent(new Event("template-loaded"));
+//         });
 // }
-modal.onclick = function() { 
-    modal.style.display = "none";
-}
 
 
-}
+// window.addEventListener("template-loaded", initJsToggle);
+
+// function initJsToggle() {
+//     $(".js-toggle").forEach((button) => {
+//         const target = button.getAttribute("toggle-target");
+//         if (!target) {
+//             document.body.innerText = `Cần thêm toggle-target cho: ${button.outerHTML}`;
+//         }
+//         button.onclick = () => {
+//             if (!$(target)) {
+//                 return (document.body.innerText = `Không tìm thấy phần tử "${target}"`);
+//             }
+//             const isHidden = $(target).classList.contains("hide");
+
+//             requestAnimationFrame(() => {
+//                 $(target).classList.toggle("hide", !isHidden);
+//                 $(target).classList.toggle("show", isHidden);
+//             });
+//         };
+//     });
+// }
+
+
+//images Modal
+// window.onload = function(){ 
+//     // Lấy phần Modal
+
+
+// }
+
+
 
 
 
